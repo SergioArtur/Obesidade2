9d685145d219d04a0a358781df382db20829bf9d
SergioArtur <sergio2505@yahoo.com.br>
2019-09-18 21:47:17 -0300

    Aula4

--- app/src/main/java/com/example/obesidade/MainActivity.java
+++ app/src/main/java/com/example/obesidade/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.obesidade;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,17 +21,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //                           Log.d("button", "Tocou em Calcular" );
                 Intent intent = new Intent(MainActivity.this, ResultadoActivity.class);
-                startActivity(intent);
 
-                intent.putExtra("Altura", 1.65);
-                intent.putExtra("Peso", 86.2);
-                startActivity(intent);
+                Double altura = getAltura();
+                Double peso = getPeso();
+
+                if (altura <= 0 || peso <= 0){
+                    showMessage("Insira o Peso/Altura e que sejam diferentes de Zero!");
+                } else{
+                    intent.putExtra("Peso", getPeso());
+                    intent.putExtra("Altura", getAltura());
+
+                    startActivity(intent);
+                }
+
+
             }
         });
     }
+
+    private Double getPeso(){
+
+        try{
+            EditText text = findViewById(R.id.editTextPeso);
+            Double peso = Double.parseDouble(text.getText().toString());
+
+            return peso;
+        }catch (Exception e){
+            return 0.0;
+         }
+    }
+
+    private Double getAltura(){
+
+        try{
+            EditText text = findViewById(R.id.editTextAltura);
+            Double altura = Double.parseDouble(text.getText().toString());
+
+            return altura;
+        }catch (Exception e){
+             return 0.0;
+            }
+
+    }
+
+    private void showMessage (String msg){
+
+        Context context = getApplicationContext();
+        String text = msg;
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+
 }
--- app/src/main/java/com/example/obesidade/ResultadoActivity.java
+++ app/src/main/java/com/example/obesidade/ResultadoActivity.java
@@ -3,6 +3,7 @@ package com.example.obesidade;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 public class ResultadoActivity extends AppCompatActivity {
 
     private TextView textView;
+    private TextView textViewMensagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,17 +19,75 @@ public class ResultadoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_resultado);
 
         textView = findViewById(R.id.textViewResultado);
+        textViewMensagem = findViewById(R.id.textViewMensagem);
 
+        Double imc = calculaImc();
+        String sIMC = getStringIMC(imc);
+
+        if (imc >= 30){
+            getWindow().getDecorView().setBackgroundColor(Color.RED);
+        } else{
+            getWindow().getDecorView().setBackgroundColor(Color.GREEN);
+        }
+
+        textView.setText(sIMC);
+        textViewMensagem.setText(avaliaIMC(imc));
+
+        /*Log.d("teste", altura.toString());
+        Log.d("teste", peso.toString());
+        Log.d("teste", imc.toString());*/
+
+    }
+
+    private Double calculaImc() {
         Intent intent = getIntent();
         Double altura = intent.getExtras().getDouble("Altura");
         Double peso = intent.getExtras().getDouble("Peso");
 
         Double imc = peso / (altura * altura);
-        textView.setText(imc.toString());
 
-        Log.d("teste", altura.toString());
-        Log.d("teste", peso.toString());
-        Log.d("teste", imc.toString());
+        return imc;
+    }
 
+    private String getStringIMC(Double imc) {
+
+        String sIMC = imc.toString();
+
+        int i = sIMC.indexOf('.');
+        sIMC = sIMC.substring(0,i+2);
+
+        return sIMC;
+    }
+
+    private String avaliaIMC(Double imc){
+
+        String resultado = "";
+
+        if (imc < 18.5)
+        {
+            resultado = "Abaixo do Peso";
+        }
+        else if (imc > 18.5 && imc < 24.9)
+        {
+            resultado = "Você está dentro do Peso";
+        }
+        else if (imc > 25 && imc < 29.9)
+        {
+            resultado = "Sobrepeso";
+        }
+        else if (imc > 30 && imc < 34.9)
+        {
+            resultado = "Obesidade Grau 1";
+        }
+        else if (imc > 35 && imc < 39.9)
+        {
+            resultado = "Obesidade Grau 2";
+        }
+        else
+        {
+            resultado = "Obesidade Grau 3";
+        }
+
+        return resultado;
     }
 }
